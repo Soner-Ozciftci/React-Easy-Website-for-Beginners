e0e86dfda8601f875cab50d25639e6d288484525
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-22 01:32:07 +0200

    import Fa

---
+++ src/footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FaGithub ,FaInstagram,FaTwitter} from "react-icons/fa";
+
+export default function footer() {
+  return (
+  <footer>
+    <div className='container'>
+<div className='social'>
+    <div className='soci'><FaGithub/></div>
+    <div className='soci'><FaInstagram/></div>
+    <div className='soci'><FaTwitter/></div>
+</div>
+<div className='copyright'>
+    Copyright &copy; 2023 Soner Ozciftci
+</div>
+    </div>
+    </footer>
+  )
+}
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./main.css";
+
+export default function main() {
+  return (
+    <div className="site-info">
+      <div className="title">SONER OZCİFTCİ</div>
+      <div className="sub-title">FRONT END DEVELOPER</div>
+    </div>
+  );
+}
